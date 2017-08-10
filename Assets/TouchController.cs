fecccd891594c25dda0aa6457a84c93901f25c36
carl <carllelandtaylor@gmail.com>
2017-08-10 16:08:20 -0700

    Added basic cube left and right hands attached to Touch controller input.

---
+++ Assets/TouchController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class TouchController : MonoBehaviour {
+    public OVRInput.Controller AssignedController;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = OVRInput.GetLocalControllerPosition(AssignedController);
+        transform.localRotation = OVRInput.GetLocalControllerRotation(AssignedController);
+    }
+}
