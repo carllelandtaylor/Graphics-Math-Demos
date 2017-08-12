7029ebed876db21fb2d36fe152e69073d157847f
carl <carllelandtaylor@gmail.com>
2017-08-11 19:10:02 -0700

    The touch controller script now updates the attached GO via the Rigidbody on the GO if one exists, to fix issues where the physics system didn't know that the object was moving.

--- Assets/TouchController.cs
+++ Assets/TouchController.cs
@@ -2,10 +2,36 @@
 
 public class TouchController : MonoBehaviour {
     public OVRInput.Controller AssignedController;
-	
-	// Update in FixedUpdate since this controller drives a model with a physics collider.
-	void FixedUpdate () {
-        transform.localPosition = OVRInput.GetLocalControllerPosition(AssignedController);
-        transform.localRotation = OVRInput.GetLocalControllerRotation(AssignedController);
+    private Rigidbody RigidbodyToDrive;
+
+    private void Start()
+    {
+        RigidbodyToDrive = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate () {
+        // Update the transform in FixedUpdate so that if the GO we're controlling has a physics
+        // collider, it will update its position as often as other physics calcs happen.
+        UpdateTransformFromController();
+    }
+
+    void UpdateTransformFromController()
+    {
+        var localControllerPosition = OVRInput.GetLocalControllerPosition(AssignedController);
+        var localControllerRotation = OVRInput.GetLocalControllerRotation(AssignedController);
+
+        // If this script is driving a GO with a Rigidbody, move the GO via the Rigidbody so that
+        // the physics system knows about the movement.
+        // Else if this GO doesn't have a Rigidbody, just move the GO normally.
+        if (RigidbodyToDrive != null)
+        {
+            RigidbodyToDrive.MovePosition(transform.parent.position + localControllerPosition);
+            RigidbodyToDrive.MoveRotation(transform.parent.rotation * localControllerRotation);
+        }
+        else
+        {
+            transform.localPosition = localControllerPosition;
+            transform.localRotation = localControllerRotation;
+        }
     }
 }
